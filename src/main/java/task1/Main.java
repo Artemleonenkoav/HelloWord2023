341cae8727c3123481bf4ffddf27ef539204bd3e
Leon <leonenkoavfin@gmail.com>
2023-11-02 20:02:45 +0200

    init project

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -5,16 +5,14 @@ public class Main {
         Bmw bmw = new Bmw("Ferari","Pegeot");
         Bmw bmw1 = new Bmw("Bmw","M500");
         Audi audi = new Audi("Fiat","Tipo");
-        Car car = new Car("Fiat","500X");
+        Car car = new Car("Seat","Leon");
         Car car1 = new Car("Fiat","500X");
 
-//        System.out.println(car);
-//        System.out.println(car1);
-//        System.out.println(bmw1);
+        System.out.println(car);
+        System.out.println(car1);
+        System.out.println(bmw1);
 
         System.out.println(car.equals(car1));
 
     }
-
-
 }
