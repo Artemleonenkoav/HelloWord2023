2fb8a690996e4a4d74d94450a33b7ed8cbcc3b27
Leon <leonenkoavfin@gmail.com>
2023-11-08 21:17:07 +0200

    Add class - Product

--- src/main/java/task1/Product.java
+++ src/main/java/task1/Product.java
@@ -6,18 +6,17 @@ import java.util.Iterator;
 public class Product {
     public static void main(String[] args) {
         ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("Banan");
+        arrayList.add("Bananas");
         arrayList.add("Lime");
         arrayList.add("Pepsi");
         arrayList.add("Cola");
 
         Iterator<String> iterator = arrayList.iterator();
         while (iterator.hasNext()) {
-            System.out.println(iterator.next());
+            if (iterator.next().equals("Pepsi")) {
+                iterator.remove();
+            }
+            System.out.println(arrayList);
         }
-//        System.out.println(arrayList);
-//        for (int i = 0; i < 20 ; i++){
-//            arrayList.add(i);
-//    }
     }
 }
